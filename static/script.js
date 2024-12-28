494e7f53a02c322b803d280257b3846d9d0a9d34
MarcosxD <marksilva147@hotmail.com>
2024-12-28 12:24:19 -0300

    start9

--- static/script.js
+++ static/script.js
@@ -74,3 +74,52 @@ document.querySelectorAll(".value-btn").forEach(button => {
     });
 });
 
+
+function countdownToNewYear() {
+    const now = new Date();
+    const nextYear = now.getFullYear() + 1;
+    const newYear = new Date(`January 1, ${nextYear} 00:00:00`).getTime();
+
+    const emergencyButton = document.getElementById("emergencyButton");
+
+    emergencyButton.disabled = true;
+    emergencyButton.style.cursor = "not-allowed";
+    emergencyButton.textContent = "Aguardando...";
+
+    const interval = setInterval(() => {
+        const currentTime = new Date().getTime();
+        const timeLeft = newYear - currentTime;
+
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+
+            document.getElementById("days").innerText = "00";
+            document.getElementById("hours").innerText = "00";
+            document.getElementById("minutes").innerText = "00";
+            document.getElementById("seconds").innerText = "00";
+
+            emergencyButton.disabled = false;
+            emergencyButton.classList.add("active");
+            emergencyButton.textContent = "Ativar Emergência";
+            emergencyButton.style.cursor = "pointer";
+
+ 
+            emergencyButton.addEventListener("click", () => {
+                window.location.href = "https://www.google.com.br/"; //linkkkkkkk
+            });
+            return;
+        }
+
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days < 10 ? "0" + days : days;
+        document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
+        document.getElementById("minutes").innerText = minutes < 10 ? "0" + minutes : minutes;
+        document.getElementById("seconds").innerText = seconds < 10 ? "0" + seconds : seconds;
+    }, 1000);
+}
+
+countdownToNewYear();
